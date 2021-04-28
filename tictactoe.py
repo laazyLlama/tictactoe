7a25b7d6f33c8e290f972d8a103ea0a8d4f38b6b
laazyLlama <laazy.llama@gmail.com>
2021-04-28 22:10:46 +0200

    added functionality to print the board

--- tictactoe.py
+++ tictactoe.py
@@ -6,3 +6,24 @@
 #Date Created:     28/04/2021
 #Last Changes:     28/04/2021
 #------------------------------------------------------------------------------
+
+#     A   B   C
+#   +---+---+---+
+# 1 |   |   |   |
+#   +---+---+---+
+# 2 |   |   |   |
+#   +---+---+---+
+# 3 |   |   |   |
+#   +---+---+---+
+
+def print_horizontal_line():
+    print("   +---+---+---+")
+
+def print_board():
+    print("     A   B   C  ")
+    print_horizontal_line()
+    for i in range(3):
+        print(" " + str(i + 1) +" |   |   |   |")
+        print_horizontal_line()
+
+print_board()
